52ff5a10d35fdbde11405e1e79c45f4b3f4ec235
blo <lo.bryan8@gmail.com>
2014-10-18 03:58:48 -0400

    added a gitignore for bins. added bash scripts to do compiling and running

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -d bin -sourcepath src -cp lib/jsoup-1.8.1.jar src/exilon.java
--- src/exilon.java
+++ src/exilon.java
@@ -1,3 +1,5 @@
+import org.jsoup.*;
+
 public class exilon {
 
 	public static void main (String[] args) {
