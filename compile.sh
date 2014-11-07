f9006efe09716c15ac3ec89cb5338c5e4131370a
blo <lo.bryan8@gmail.com>
2014-11-06 22:23:44 -0500

    added javax.mail.jar to compile/run.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-javac -d bin -sourcepath src -cp lib/jsoup-1.8.1.jar:lib/gson-2.3.jar src/Stockr.java
+javac -d bin -sourcepath src -cp lib/javax.mail.jar:lib/jsoup-1.8.1.jar:lib/gson-2.3.jar src/Stockr.java
